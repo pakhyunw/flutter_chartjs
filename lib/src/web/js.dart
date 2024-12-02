c4db2416876ca303da9865aa9a5cd93f5271535d
hyunwoo pak <88700361+pakhyunw@users.noreply.github.com>
2024-12-02 09:26:25 +0900

    ..

--- lib/src/web/js.dart
+++ lib/src/web/js.dart
@@ -1,8 +1,9 @@
 library ChartJs_js_web;
 
-import 'package:flutter_js/flutter_js.dart';
+import 'package:js/js.dart';
 
 ///
 /// A JavaScript module for eval function.
 ///
+@JS('eval')
 external void eval(String code);
